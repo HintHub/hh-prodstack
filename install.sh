8f1dcb88ea15b9ab5aa4f92e8edb73be1bc5c073
karim.saad@iubh.de <karim.saad@iubh.de>
2022-01-11 01:10:41 +0100

    added plan

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Install Script 01/2021 | Hinthub | Karim S
 
 # store git creds
 git config credential.helper 'store'
@@ -7,5 +8,12 @@ git config credential.helper 'store'
 cd "nginx/www/"
 git clone  "https://git-se.iubh.de/ali-kemal.yalama/Hinthub.git" .
 
-cp ../../copy/env.local .env.local
-# 
+# step 1 copy docker-compose.yaml
+# step 2 replace values
+# step 3 copy env.local to nginx/www/.env.local
+# step 4 replace values
+
+
+# run install.sh
+
+# in copy func cp ../../copy/env.local .env.local
